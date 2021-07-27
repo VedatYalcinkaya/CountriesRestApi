e837fe544cf4d1337a865880e49746f9b16a080b
VedatYalcinkaya <vedatylcnky@gmail.com>
2021-07-27 15:20:28 +0300

    inialitize commit

---
+++ src/test/java/com/yalcinkayavedat/countries_app/CountriesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yalcinkayavedat.countries_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
